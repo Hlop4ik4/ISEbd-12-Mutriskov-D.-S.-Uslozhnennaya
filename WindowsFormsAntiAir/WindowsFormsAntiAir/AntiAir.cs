fff2c4194a155d03e06580332b679f1b4f14ac7c
Hlop4ik4 <hlop4ik773@gmail.com>
2021-12-23 08:55:00 +0400

    gfdgfd

--- WindowsFormsAntiAir/WindowsFormsAntiAir/AntiAir.cs
+++ WindowsFormsAntiAir/WindowsFormsAntiAir/AntiAir.cs
@@ -7,75 +7,22 @@ using System.Drawing;
 
 namespace WindowsFormsAntiAir
 {
-	class AntiAir
+	class AntiAir : ArmoredCar
 	{
-		private float _startPosX;
-		private float _startPosY;
-		private int _pictureWidth;
-		private int _pictureHeight;
-		private readonly int AAWidth = 280;
-		private readonly int AAHeight = 117;
 		private DopClass dopClass;
-		public int MaxSpeed { private set; get; }
-		public float Weight { private set; get; }
-		public Color MainColor { private set; get; }
 		public Color DopColor { private set; get; }
 		public bool StarEmblem { private set; get; }
 		public bool Gun { private set; get; }
 
-		public void Init(int maxSpeed, float weight, Color mainColor, Color dopColor,
-bool starEmblem, bool gun, int numberOfGuns)
+		public AntiAir(int maxSpeed, float weight, Color mainColor, Color dopColor, bool starEmblem, bool gun, int numberOfGuns) : base(maxSpeed, weight, mainColor, 280, 117)
 		{
-			MaxSpeed = maxSpeed;
-			Weight = weight;
-			MainColor = mainColor;
 			DopColor = dopColor;
 			StarEmblem = starEmblem;
 			Gun = gun;
 			dopClass = new DopClass(numberOfGuns, DopColor);
 		}
 
-		public void SetPosition(int x, int y, int width, int height)
-		{
-			_startPosX = x;
-			_startPosY = y;
-			_pictureHeight = height;
-			_pictureWidth = width;
-		}
-
-		public void MoveTransport(Direction direction)
-		{
-			float step = MaxSpeed * 100 / Weight;
-			switch (direction)
-			{
-				case Direction.Right:
-					if (_startPosX + step < _pictureWidth - AAWidth)
-					{
-						_startPosX += step;
-					}
-					break;
-				case Direction.Left:
-					if(_startPosX - step > 0)
-					{
-						_startPosX -= step;
-					}
-					break;
-				case Direction.Up:
-					if (_startPosY - step > 0)
-					{
-						_startPosY -= step;
-					}
-					break;
-				case Direction.Down:
-					if (_startPosY + step < _pictureHeight - AAHeight)
-					{
-						_startPosY += step;
-					}
-					break;
-			}
-		}
-
-		public void DrawAA(Graphics g)
+		public override void DrawTransport(Graphics g)
 		{
 			if (Gun)
 			{
@@ -84,22 +31,7 @@ bool starEmblem, bool gun, int numberOfGuns)
 				Pen DopPen = new Pen(DopColor);
 				g.DrawLine(DopPen, _startPosX + 130, _startPosY + 17, _startPosX + 70, _startPosY - 3);
 			}
-			Pen pen = new Pen(Color.Black);
-			g.DrawArc(pen, _startPosX, _startPosY + 62, 50, 50, 90, 180);
-			g.DrawArc(pen, _startPosX + 230, _startPosY + 62, 50, 50, 270, 180);
-			g.DrawLine(pen, _startPosX + 25, _startPosY + 112, _startPosX + 255, _startPosY + 112);
-			g.DrawEllipse(pen, _startPosX + 5, _startPosY + 64, 45, 45);
-			g.DrawEllipse(pen, _startPosX + 231, _startPosY + 64, 45, 45);
-			g.DrawEllipse(pen, _startPosX + 60, _startPosY + 82, 30, 30);
-			g.DrawEllipse(pen, _startPosX + 100, _startPosY + 82, 30, 30);
-			g.DrawEllipse(pen, _startPosX + 140, _startPosY + 82, 30, 30);
-			g.DrawEllipse(pen, _startPosX + 180, _startPosY + 82, 30, 30);
-			Brush br = new SolidBrush(MainColor);
-			g.FillRectangle(br, _startPosX + 80, _startPosY + 17, 100, 40);
-			g.FillRectangle(br, _startPosX + 10, _startPosY + 47, 260, 22);
-			g.FillEllipse(br, _startPosX + 130, _startPosY + 57, 20, 20);
-			g.FillEllipse(br, _startPosX + 90, _startPosY + 57, 20, 20);
-			g.FillEllipse(br, _startPosX + 170, _startPosY + 57, 20, 20);
+			base.DrawTransport(g);
 			if (StarEmblem)
 			{
 				Brush brDop = new SolidBrush(DopColor);
---
+++ WindowsFormsAntiAir/WindowsFormsAntiAir/ArmoredCar.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsAntiAir
+{
+	public class ArmoredCar : Vehicle
+	{
+		protected readonly int AAWidth = 280;
+		protected readonly int AAHeight = 100;
+
+		public ArmoredCar(int maxSpeed, float weight, Color mainColor)
+		{
+			MaxSpeed = maxSpeed;
+			Weight = weight;
+			MainColor = mainColor;
+		}
+
+		protected ArmoredCar(int maxSpeed, float weight, Color mainColor, int AAWidth, int AAHeight)
+		{
+			MaxSpeed = maxSpeed;
+			Weight = weight;
+			MainColor = mainColor;
+			this.AAWidth = AAWidth;
+			this.AAHeight = AAHeight;
+		}
+
+		public override void MoveTransport(Direction direction)
+		{
+			float step = MaxSpeed * 100 / Weight;
+			switch (direction)
+			{
+				case Direction.Right:
+					if (_startPosX + step < _pictureWidth - AAWidth)
+					{
+						_startPosX += step;
+					}
+					break;
+				case Direction.Left:
+					if (_startPosX - step > 0)
+					{
+						_startPosX -= step;
+					}
+					break;
+				case Direction.Up:
+					if (_startPosY - step > 0)
+					{
+						_startPosY -= step;
+					}
+					break;
+				case Direction.Down:
+					if (_startPosY + step < _pictureHeight - AAHeight)
+					{
+						_startPosY += step;
+					}
+					break;
+			}
+		}
+
+		public override void DrawTransport(Graphics g)
+		{
+			Pen pen = new Pen(Color.Black);
+			g.DrawArc(pen, _startPosX, _startPosY + 62, 50, 50, 90, 180);
+			g.DrawArc(pen, _startPosX + 230, _startPosY + 62, 50, 50, 270, 180);
+			g.DrawLine(pen, _startPosX + 25, _startPosY + 112, _startPosX + 255, _startPosY + 112);
+			g.DrawEllipse(pen, _startPosX + 5, _startPosY + 64, 45, 45);
+			g.DrawEllipse(pen, _startPosX + 231, _startPosY + 64, 45, 45);
+			g.DrawEllipse(pen, _startPosX + 60, _startPosY + 82, 30, 30);
+			g.DrawEllipse(pen, _startPosX + 100, _startPosY + 82, 30, 30);
+			g.DrawEllipse(pen, _startPosX + 140, _startPosY + 82, 30, 30);
+			g.DrawEllipse(pen, _startPosX + 180, _startPosY + 82, 30, 30);
+			Brush br = new SolidBrush(MainColor);
+			g.FillRectangle(br, _startPosX + 80, _startPosY + 17, 100, 40);
+			g.FillRectangle(br, _startPosX + 10, _startPosY + 47, 260, 22);
+			g.FillEllipse(br, _startPosX + 130, _startPosY + 57, 20, 20);
+			g.FillEllipse(br, _startPosX + 90, _startPosY + 57, 20, 20);
+			g.FillEllipse(br, _startPosX + 170, _startPosY + 57, 20, 20);
+		}
+	}
+}
